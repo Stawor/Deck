a8bb540ac8c5b74c41db0d8342f2ecd37a1b9df0
Stawor <staworkogabriel@gmail.com>
2023-10-17 22:55:35 +0200

    added delete

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import DeckModel from "./models/Deck.js";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
@@ -9,6 +10,11 @@ const port = 3000;
 await mongoose.connect(process.env.MONGO_URL);
 
 app.use(express.json());
+app.use(cors());
+
+app.get("/decks", async (req, res) => {
+	res.json();
+});
 
 app.post("/decks", async (req, res) => {
 	console.log(req.body);
@@ -18,7 +24,11 @@ app.post("/decks", async (req, res) => {
 	const a = await newDeck.save();
 	res.send(a);
 });
-
+app.delete("/decks/:deckId", async (req, res) => {
+	const deckId = req.params.deckId;
+	const deck = await DeckModel.findByIdAndDelete(deckId);
+	res.json(deck);
+});
 app.listen(port, () => {
 	console.log(` app listening on port ${port}`);
 });
